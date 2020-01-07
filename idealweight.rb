6708b28ebb38a0a0957a7172b87f6beb389c3f3f
carden <carde.ruby@gmail.com>
2020-01-08 02:26:15 +0300

    optim

--- idealweight.rb
+++ idealweight.rb
@@ -1,7 +1,5 @@
 puts "Укажите Ваше Имя:"
 name = gets.chomp
-
 puts "Укажите Ваш рост:"
 height = gets.chomp.to_f
-
 puts height <= 110 ? "#{name} Ваш вес уже оптимальный" : "#{name} Ваш оптимальный вес: #{(height - 110) * 1.15}"
