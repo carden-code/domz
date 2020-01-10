383706fa1917267e225f4046dff839604e33cf06
carden <carde.ruby@gmail.com>
2020-01-11 02:25:50 +0300

    Metod deleniya,edit4

--- metods.rb
+++ metods.rb
@@ -1,13 +1,10 @@
 def delenie(delimoe = 1, delitel = 1)
-  ostatok = (delimoe.to_i % delitel.to_i)
-  return ostatok.to_i
+  ostatok = delimoe.to_i % delitel.to_i
+  return ostatok
 end
-
 puts "Деление по модулю"
-
 puts "Делимое:"
 delimoe = gets.chomp.to_i
-
 puts "Делитель:"
 delitel = gets.chomp.to_i
 puts("Остаток равен: #{delenie(delimoe, delitel)}")
