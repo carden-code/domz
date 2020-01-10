bbd35b5e5669d5c014494490ded8e0fa59847474
carden <carde.ruby@gmail.com>
2020-01-11 01:02:51 +0300

    Delenie po modulu

---
+++ metods.rb
@@ -0,0 +1,9 @@
+puts "Деление по модулю"
+puts "Делимое:"
+a = gets.chomp.to_i
+puts "Делитель:"
+b = gets.chomp.to_i
+puts "Делимое:a=#{a}"
+puts "Делитель:b=#{b}"
+puts "форула : a % b"
+puts "Остаток: #{ a % b }"
