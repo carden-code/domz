376ec93bc391f451f3c2c64cff5fc8084e9693e1
carden <carde.ruby@gmail.com>
2020-01-11 22:55:17 +0300

    Probniy kod

--- metods2.rb
+++ metods2.rb
@@ -1,13 +1,27 @@
 def delenie(delimoe = 1, delitel = 1)
-  ostatok = delimoe.to_i % delitel.to_i
+  ostatok = delimoe % delitel
   return ostatok
 end
-def delenie_s_ostatkom(delimoe = 1, delitel = 1)
-  rezultat = delimoe / delitel
+def del(delimoe, delitel)
+  itog = delimoe / delitel
+  return itog
+end
+def umnogenie(delitel, ostatok)
+  rezultat = delitel * ostatok
   return rezultat
+end
+def vichet(delimoe, rezultat)
+  summa = delimoe - rezultat
+  return summa
+end
 puts "Деление по модулю"
-puts "Делимое:"
+puts "Введите цело положительное число больше 0:"
 delimoe = gets.chomp.to_i
 puts "Делитель:"
 delitel = gets.chomp.to_i
+itog = del(delimoe, delitel)
+rezultat = umnogenie(itog, delitel)
 puts("Остаток равен: #{delenie(delimoe, delitel)}")
+puts("Модуль числа:#{del(delimoe, delitel)}")
+puts("Число без остатка:#{umnogenie(itog, delitel)}")
+puts("Остаток:#{vichet(delimoe, rezultat)}")
