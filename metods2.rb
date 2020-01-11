eb85b766925cff8ea419e5c3b55e2718a7c3fb9e
carden <carde.ruby@gmail.com>
2020-01-11 23:53:08 +0300

    kod gotov1

--- metods2.rb
+++ metods2.rb
@@ -4,8 +4,9 @@ def delenie_po_modulu(delimoe, delitel)
   ostatok = delimoe - bezostatka
   return [modul, bezostatka, ostatok]
 end
+puts "Деление по модулю"
 puts "Укажите целое положительное число больше 0:"
 delimoe = gets.chomp.to_i
-puts "Делитель"
+puts "Делитель:"
 delitel = gets.chomp.to_i
 puts("Модуль числа:#{delenie_po_modulu(delimoe, delitel)[0]}, Число без остатка:#{delenie_po_modulu(delimoe,delitel)[1]}, Остаток:#{delenie_po_modulu(delimoe, delitel)[2]}")
