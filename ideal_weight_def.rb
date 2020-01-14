b2fab9e975d3df2d136897a6d2ecdfce2aa22fb3
carden <carde.ruby@gmail.com>
2020-01-14 23:24:37 +0300

    rename, change to english

--- ideal_weight_def.rb
+++ ideal_weight_def.rb
@@ -8,4 +8,4 @@ name = gets.chomp
 puts "Indicate your height:"
 height = gets.chomp.to_f
 puts "#{name}, your ideal weight: #{idealves(height)[0]}"
-puts idealves(height)[1] ? "#{name}, your weight is already optimal" : "#{name}, your ideal weight: #{idealves(height)[0]}"
+puts idealves(height)[1] ? "#{name}, your weight is already optimal " : "#{name}, your ideal weight: #{idealves(height)[0]}"
