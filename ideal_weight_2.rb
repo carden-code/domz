ceeff5a9293fa5e7baaec76e6e6ba860c3c278b7
carden <carde.ruby@gmail.com>
2020-01-14 23:26:11 +0300

    rename, change to english

--- ideal_weight_2.rb
+++ ideal_weight_2.rb
@@ -3,4 +3,4 @@ name = gets.chomp
 puts "Indicate your height:"
 height = gets.chomp.to_f
 puts "#{name}, your ideal weight: #{(height - 110) * 1.15}"
-puts((height - 110) * 1.15).negative? ? "#{name}, your weight is already optimal" : "#{name}, your ideal weight: #{(height - 110) * 1.15}"
+puts((height - 110) * 1.15).negative? ? "#{name}, your weight is already optimal " : "#{name}, your ideal weight: #{(height - 110) * 1.15}"
