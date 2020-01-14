569f9371a1f07ec1ef9dbd4a74ecaffd412e04e4
carden <carde.ruby@gmail.com>
2020-01-14 22:07:33 +0300

    update

--- editfile.rb
+++ editfile.rb
@@ -4,7 +4,8 @@ print 'Please enter your height in centimeters: '
 tall = gets.to_i
 weight = tall - 110
 if weight.negative? || weight.zero?
-  puts "Your ideal weight: #{weight} kg."
+  puts "Weight: #{weight}"
+  puts "#{name}, Your weight is already optimal."
 else
   puts "#{name}, Your optimal weight is: #{weight} kg."
 end
