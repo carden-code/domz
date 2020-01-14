44e85e47295b2b9464847318cab667d54cf1d287
carden <carde.ruby@gmail.com>
2020-01-14 21:48:31 +0300

    update

--- editfile.rb
+++ editfile.rb
@@ -1,15 +1,9 @@
 print 'Please enter your name: '
 name = gets.chomp.capitalize
-
 print 'Please enter your height in centimeters: '
 tall = gets.to_i
-
 weight = tall - 110
-
-if weight.negative?
-  puts 'Your weight is already optimal.'
-elsif weight == 0
-  puts 'Your weight is already optimal.'
+if weight.negative? || weight.zero?
 else
   puts "#{name}, Your optimal weight is: #{weight} kg."
 end
