d47487ffa14a8b2557b30924799ba4fcd745b8ed
carden <carde.ruby@gmail.com>
2020-01-21 22:42:58 +0300

    error correction

--- 2.part/vowels2.rb
+++ 2.part/vowels2.rb
@@ -1,19 +1,13 @@
 puts 'Want to see glass letters or consonants?'
-puts "v - vowels; c - consonants"
+puts "v - vowels; c - consonants."
 alphabet = ('a'..'z').to_a
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
-              'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['w', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+              'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 c = {}
 v = {}
 alphabet.each_with_index do |x, i|
   v[i + 1] = x if vowels.include? x
-alphabet.each_with_index do |a, b|
-  c[b + 1] = a if consonants.include? a
+  c[i + 1] = x if consonants.include? x
 end
-end
-if gets.chomp == "v"
-  puts "vowels #{v}"
-else
-  puts "consonants :#{c}"
-end
+puts gets.chomp == 'v' ? "vowels: #{v}" : "consonants: #{c}"
