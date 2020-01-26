31c6f1339c238ffae61e0c908b75a3ffc8f1d74c
carden <carde.ruby@gmail.com>
2020-01-26 23:28:42 +0300

    each_with_index and def

--- 2.part/date_search.rb
+++ 2.part/date_search.rb
@@ -1,1 +1,16 @@
-
+puts 'Please enter the day (1 - 31):'
+day = gets.chomp.to_i
+puts 'Please enter a month (1 - 12):'
+month = gets.chomp.to_i
+puts 'Please enter the year:'
+year = gets.chomp.to_i
+def whole_d(year, day, month)
+  days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  days[1] = 29 if ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
+  sum = 0
+  days.each_with_index do |value, index|
+    sum += value if index + 1 < month
+  end
+  puts sum + day
+end
+puts "#{whole_d(year, day, month)} - day since the beginning of the year"
