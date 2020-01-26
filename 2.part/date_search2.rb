12dc04d862fd67545d9148f1cd0cd9af2642b421
carden <carde.ruby@gmail.com>
2020-01-27 01:41:51 +0300

    First part of Salami

--- 2.part/date_search2.rb
+++ 2.part/date_search2.rb
@@ -1,4 +1,5 @@
 # the beginning of first part of code
+# the program asks the user for 3 variables (day, month, year) and displays them
 puts 'Please enter the day of the month:'
 day = gets.chomp.to_i
 puts 'Please enter the month number:'
@@ -7,7 +8,10 @@ puts 'Please enter the year:'
 year = gets.chomp.to_i
 puts "The date you entered: #{day}.#{month}.#{year}"
 # end of first part of code
+
 # the beginning of second part of code
+# in this part of the program using the method,
+# the program determines the leap year or not
 def leap?(year)
   ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
 end
