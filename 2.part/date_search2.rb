caf4197ec1dd09cd65cf3a96997d41c10a0e59d8
carden <carde.ruby@gmail.com>
2020-01-27 01:17:55 +0300

    First part of Salami

--- 2.part/date_search2.rb
+++ 2.part/date_search2.rb
@@ -4,6 +4,3 @@ puts 'Please enter the month number:'
 month = gets.chomp.to_i
 puts 'Please enter the year:'
 year = gets.chomp.to_i
-def is_leap?(year)
-  ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
-end
