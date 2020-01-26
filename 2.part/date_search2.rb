85fe4bd5c94918fa876b1ea2e22eebdefa3b3441
carden <carde.ruby@gmail.com>
2020-01-27 01:19:53 +0300

    First part of Salami

--- 2.part/date_search2.rb
+++ 2.part/date_search2.rb
@@ -1,6 +1,8 @@
+# the beginning of first part of code
 puts 'Please enter the day of the month:'
 day = gets.chomp.to_i
 puts 'Please enter the month number:'
 month = gets.chomp.to_i
 puts 'Please enter the year:'
 year = gets.chomp.to_i
+# end of first part of code
