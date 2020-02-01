e0c421969b8251025edb437ff6a1f7bbbc219c8e
carden <carde.ruby@gmail.com>
2020-02-01 20:48:27 +0300

    the first part of the assignment

---
+++ 2.part/amount_of_purchases.rb
@@ -0,0 +1,20 @@
+product_name = []
+unit_price = []
+quantity_of_goods = []
+user_input = nil
+puts 'Введите пожалуйста данны о товаре(Название, Стоимость, Колличество)'
+while user_input != 'stop' do
+  puts 'Введите пожалуйста название товара'
+  user_input = gets.chomp
+  product_name << user_input
+  puts 'Введите пожалуйста стоимость товара'
+  user_input = gets.chomp.to_f
+  unit_price << user_input
+  puts 'Введите пожалуйста количество товара'
+  user_input = gets.chomp.to_f
+  quantity_of_goods << user_input
+  puts 'Если товаров больше нет введите "stop" если есть нажмите "Enter"'
+  user_input = gets.chomp
+end
+puts "Названия товаров#{product_name}, Стоимость товаров за штуку#{unit_price},
+Колличество товаров#{quantity_of_goods}"
