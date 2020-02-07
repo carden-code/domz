bda0e48e499018edd556c02bac7595160b5f35c8
carden <carde.ruby@gmail.com>
2020-02-07 20:55:28 +0300

    edit

--- 2.part/test.rb
+++ 2.part/test.rb
@@ -3,4 +3,4 @@ puts 'Enter a positive integer to search for it in the array.'
 num = gets.chomp.to_i
 result = false
 r.each { |elem| result = true if num == elem }
-puts result == true ? 'The number is in the array.' : 'The number is missing.'
+puts result ? 'The number is in the array.' : 'The number is missing.'
