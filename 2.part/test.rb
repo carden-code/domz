c2539034939711d7ba6135813812155c1b47c1e3
carden <carde.ruby@gmail.com>
2020-02-07 20:27:15 +0300

    edit

--- 2.part/test.rb
+++ 2.part/test.rb
@@ -1,12 +1,6 @@
 arr = (1..20).to_a
 puts 'Enter a positive integer to search for it in the array.'
 a = gets.chomp.to_i
-arr.each do |element|
-  if a == element
-    puts 'yes'
-    break
-  else
-    puts 'no'
-    break
-  end
-end
+result = false
+arr.each { |element| result = true if a == element }
+puts result == true ? 'The number is in the array.' : 'The number is missing.'
