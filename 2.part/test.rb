89c4990f2dfebffe8caf47636eb9ec74d03c55be
carden <carde.ruby@gmail.com>
2020-02-07 14:34:41 +0300

    code upgrade

--- 2.part/test.rb
+++ 2.part/test.rb
@@ -1,10 +1,6 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-puts 'Введите число для проверки в массиве'
+arr = (1..20)
+puts 'Enter a positive integer to search for it in the array.'
 a = gets.chomp.to_i
-result = false
-arr.each do |index|
-  if a == index
-    result = true
-  end
-end
-puts result ? 'Ваше число есть в массиве' : 'Вашего числа нет в массиве'
+res = false
+arr.each { |element| a == element && a == res = true }
+puts res ? 'Your number is in the array.' : 'Your number is not in the array.'
