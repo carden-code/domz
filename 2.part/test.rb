2fc4be75c1e9801cde1c38e0f4b7a27cc762d261
carden <carde.ruby@gmail.com>
2020-02-07 19:32:07 +0300

    edit

--- 2.part/test.rb
+++ 2.part/test.rb
@@ -1,6 +1,4 @@
-arr = (1..20)
+arr = (1..20).to_a
 puts 'Enter a positive integer to search for it in the array.'
 a = gets.chomp.to_i
-res = false
-arr.each { |element| a == element && a == res = true }
-puts res ? 'Your number is in the array.' : 'Your number is not in the array.'
+arr.each { |element| puts a == element ? 'num yes' : 'num no' }
