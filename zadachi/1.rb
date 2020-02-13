c0c4a1867c33eea261cb148f452ba291ffb301ea
carden <carden.ruby@gmail.com>
2020-02-13 23:49:06 +0300

    modification

--- zadachi/1.rb
+++ zadachi/1.rb
@@ -1,3 +1,9 @@
-arr = (1..10).to_a
-arr.each_with_index { |el, index| p "index(even) : #{el}" if index.even? }
-arr.each_with_index { |el, index| p "index(odd) : #{el}" if index.odd? }
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even = []
+odd = []
+array.each_with_index do |element, index|
+  ar = (index % 2).zero? ? even : odd
+  ar << element
+end
+p odd
+p even
