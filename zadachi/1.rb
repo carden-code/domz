a263701c85baa5fb6ccc153b1c85ed855ebe179d
carden <carden.ruby@gmail.com>
2020-02-13 23:54:38 +0300

    modification

--- zadachi/1.rb
+++ zadachi/1.rb
@@ -1,3 +1,6 @@
+# The program will loop through the array by its indices
+# and output first the values ​​of the even indices,
+# then the values ​​of the odd indices.
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even = []
 odd = []
