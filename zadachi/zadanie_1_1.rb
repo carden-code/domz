4abb5006c5cd479375ec53c92e753e922514697b
carden <carden.ruby@gmail.com>
2020-02-15 01:24:23 +0300

    second decision

--- zadachi/zadanie_1_1.rb
+++ zadachi/zadanie_1_1.rb
@@ -4,4 +4,4 @@
 a = [1, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
 even, odd = a.partition.with_index { |_, index| index.even? }
 p "Array elements with an even index - #{even}"
-p "Odd Index Array Elements - #{odd}"
+p "Array elements with odd indices - #{odd}"
