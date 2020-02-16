e2a0318f11a58f6deb8e0594258ed0273aa5c39b
carden <carden.ruby@gmail.com>
2020-02-16 21:33:46 +0300

    modification

--- zadachi/task_3.rb
+++ zadachi/task_3.rb
@@ -1,8 +1,6 @@
 # the program checks the indices of each element of the array
 # for double inequality, and displays the first element that satisfies
 # the inequality, if there are no such elements, displays an empty array.
-a = [1, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
-arr = []
-a.each_index { |i| arr << i if a[0] < i && i < a[-1] }
-p "All elements satisfying the inequality: #{arr}"
-p "First element satisfying the inequality: #{arr.first}"
+a = [-1, -2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
+x = a.index { |i| a[0] < i && i < a[-1] } || []
+p x
