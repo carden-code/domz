c5449fdb469a11df5e764742ba21017f9c659d89
carden <carden.ruby@gmail.com>
2020-02-16 22:03:38 +0300

    rindex

--- zadachi/task_4.rb
+++ zadachi/task_4.rb
@@ -1,8 +1,6 @@
 # The program checks the indices of each element of the array
 # for double inequality and displays the last item that satisfies
 # inequality, if there are no such elements, displays an empty array.
-a = [1, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
-arr = []
-a.each_index { |i| arr << i if a[0] < i && i < a[-1] }
-p "All elements satisfying the inequality: #{arr}"
-p "First element satisfying the inequality: #{arr.last}"
+a = [-1, 2, -3, -4, 2, 5, 5, 6, 7, 8, 1, 9]
+x = a.rindex { |i| a[0] < i && i < a[-1] } || []
+p x
