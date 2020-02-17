101db5655dc494964affc3ed53942c6a54a9c0f6
carden <carden.ruby@gmail.com>
2020-02-17 21:18:02 +0300

    edit

--- zadachi/task_5.rb
+++ zadachi/task_5.rb
@@ -1,5 +1,4 @@
 # The program converts the array and adds the first element of the array
 # to even elements. The first and last element of the array remains unchanged.
-a = [4, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9, 4]
-a.each_with_index { |el, i| i[0][-1].freeze && a[el] += a[0] if el.odd? }
-p a
+a = [2, 1, 2, -2, 0, 4, 9, 8]
+a[1..-2].each { |z| puts z += a[0] if z.even? }
