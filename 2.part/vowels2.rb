0bbf15ec185c2657b8029fda0fae7277709412c9
carden <carden.ruby@gmail.com>
2020-02-18 19:27:11 +0300

    modification

--- 2.part/vowels2.rb
+++ 2.part/vowels2.rb
@@ -5,7 +5,5 @@ vowels = %w[a e i o u]
 consonants = %w[w c d f g h j k l m n p q r s t v w x y z]
 gets.chomp == 'v' ? l = vowels : l = consonants
 v = {}
-alphabet.each_with_index do |x, i|
-  v[i + 1] = x if l.include? x
-end
+alphabet.each_with_index { |x, i| v[i + 1] = x if l.include? x }
 puts "#{v}"
