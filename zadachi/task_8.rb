25f2e2e5193e282eec95a6ee3a4cd83d8cea8ff8
carden <carden.ruby@gmail.com>
2020-02-18 18:15:47 +0300

    completed

---
+++ zadachi/task_8.rb
@@ -0,0 +1,9 @@
+# The program converts the array and adds the first element of the array
+# to odd elements. The first and last element of the array remains unchanged.
+a = [2, 1, 2, -2, 0, 4, 9, 8]
+product = a.each_with_index do |_, i|
+  if i > 0 && i < a.size - 1
+    a[i] += a.first if a[i].odd?
+  end
+end
+puts "Modified array : #{product}"
