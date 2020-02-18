3407f4187ad2dce181535f17ebb113601c728eb7
carden <carden.ruby@gmail.com>
2020-02-18 17:40:32 +0300

    modification

--- zadachi/task_5.rb
+++ zadachi/task_5.rb
@@ -1,4 +1,9 @@
 # The program converts the array and adds the first element of the array
 # to even elements. The first and last element of the array remains unchanged.
 a = [2, 1, 2, -2, 0, 4, 9, 8]
-a[1..-2].each { |z| puts z += a[0] if z.even? }
+product = a.each_with_index do |_, i|
+  if i > 0 && i < a.size - 1
+    a[i] += a.first if a[i].even?
+  end
+end
+puts product
