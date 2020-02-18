ed9dfb7037d66420b05f4dd336e67e8fb9f07ccf
carden <carden.ruby@gmail.com>
2020-02-18 17:46:53 +0300

    modification

--- zadachi/task_6.rb
+++ zadachi/task_6.rb
@@ -1,4 +1,9 @@
 # The program converts the array and adds the last element of the array
 # to even elements. The first and last element of the array remains unchanged.
 a = [2, 1, 2, -2, 0, 4, 9, 8]
-a[1..-2].each { |z| puts z += a[-1] if z.even? }
+product = a.each_with_index do |_, i|
+  if i > 0 && i < a.size - 1
+    a[i] += a.last if a[i].even?
+  end
+end
+puts "Modified array : #{product}"
