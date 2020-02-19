6d9b654a95c2431d8e8638c042b638b58988aa74
carden <carden.ruby@gmail.com>
2020-02-19 20:52:59 +0300

    positive? OK

--- zadachi/task_9.rb
+++ zadachi/task_9.rb
@@ -1,5 +1,5 @@
 # The program changes all positive elements of the array
 # to the value of the minimum element.
 a = [2, 1, 2, -2, 0, 4, 9, 8]
-product = a.each_with_index { |_, i| a[i] = a.min if a[i] > 0 }
+product = a.each_with_index { |_, i| a[i] = a.min if a[i].positive? }
 puts "Modified array : #{product}"
