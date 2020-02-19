c888536f8427c2aadb7b6703e13c2e2e72aaf206
carden <carden.ruby@gmail.com>
2020-02-19 21:46:49 +0300

    rename

---
+++ zadachi/13.rb
@@ -0,0 +1,5 @@
+# The program changes all negative elements of the array
+# to the value of the min element.
+a = [2, 1, 2, -2, 0, 4, -6, 9, 8]
+product = a.each_with_index { |_, i| a[i] = a.max if a[i].negative? }
+puts "Modified array : #{product}"
