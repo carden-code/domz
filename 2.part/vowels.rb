b98e4334b144d30e1de3f1e8a46b27d77ed5d2a0
carden <carden.ruby@gmail.com>
2020-02-19 21:08:24 +0300

    edit

--- 2.part/vowels.rb
+++ 2.part/vowels.rb
@@ -1,5 +1,5 @@
 alphabet = ('а'..'я').to_a
 vowels = %w[а о и е ё э ы у ю я]
 hash = {}
-alphabet.each_with_index { |x, i| hash[i + 1] = x if vowels.include? x }
+alphabet.each_with_index { |el, i| hash[i + 1] = el if vowels.include? el }
 print hash
