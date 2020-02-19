36d84e8c4b550fb61dbecd756aa0320aaf59f5e4
carden <carden.ruby@gmail.com>
2020-02-19 21:36:23 +0300

    positive?

--- zadachi/task_8.rb
+++ zadachi/task_8.rb
@@ -2,7 +2,7 @@
 # to odd elements. The first and last element of the array remains unchanged.
 a = [2, 1, 2, -2, 0, 4, 9, 8]
 product = a.each_with_index do |_, i|
-  if i > 0 && i < a.size - 1
+  if i.positive? && i < a.size - 1
     a[i] += a.first if a[i].odd?
   end
 end
