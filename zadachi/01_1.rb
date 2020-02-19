e5cae27a6e6edef5dd301f879a5be64d96da9615
carden <carden.ruby@gmail.com>
2020-02-19 21:52:46 +0300

    index => i

--- zadachi/01_1.rb
+++ zadachi/01_1.rb
@@ -2,6 +2,6 @@
 # all the elements of the array with an even index then all the elements
 # of the array with an odd index
 a = [1, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
-even, odd = a.partition.with_index { |_, index| index.even? }
+even, odd = a.partition.with_index { |_, i| i.even? }
 p "Array elements with an even index - #{even}"
 p "Array elements with odd indices - #{odd}"
