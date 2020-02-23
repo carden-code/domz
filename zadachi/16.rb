26b1fabdaaff8325bab2f43f002fadd13d9dcd9a
carden <carden.ruby@gmail.com>
2020-02-23 22:30:55 +0300

    arifmetic progresson

--- zadachi/16.rb
+++ zadachi/16.rb
@@ -1,6 +1,6 @@
 # The program checks if the elements of the array form an arithmetic progression
 # If yes, it outputs the difference; if not, displays nil
-arr = [1, 2, 4, 6, 7, -1, 0, 22]
+arr = [2, 1, 2, 4, 6, 7, -1, 0, 22]
 d = arr[1] - arr[0]
 arr.each
 if arr.size > 2 && arr[1] == ((arr[0] + arr[2]) / 2.0)
