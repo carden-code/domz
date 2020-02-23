94b2b1cd29a75f88dca0d2e5c643303d9d646193
carden <carden.ruby@gmail.com>
2020-02-23 23:28:13 +0300

    modification

--- zadachi/01_1.rb
+++ zadachi/01_1.rb
@@ -1,7 +1,7 @@
 # The program iterates over the indices of the array and first displays
 # all the elements of the array with an even index then all the elements
 # of the array with an odd index
-a = [1, 2, 3, 4, 2, 5, 5, 6, 7, 8, 1, 9]
+a = [1, 2, 3, 4, 2, 5, -1, 0, 5, 6, 7, 8, 1, 9]
 even, odd = a.partition.with_index { |_, i| i.even? }
 p "Array elements with an even index - #{even}"
 p "Array elements with odd indices - #{odd}"
