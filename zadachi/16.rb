94cda78a93515468486bde5efdbc9cf5fb153e41
carden <carden.ruby@gmail.com>
2020-02-23 22:52:30 +0300

    modification

--- zadachi/16.rb
+++ zadachi/16.rb
@@ -2,7 +2,6 @@
 # If yes, it outputs the difference; if not, displays nil
 array = [2, 1, 2, 4, 6, 7, -1, 0, 22]
 difference = array[1] - array[0]
-array.each
 if array.size > 2 && array[1] == ((array[0] + array[2]) / 2.0)
   puts "Progression difference : #{difference}"
 else
