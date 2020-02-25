2dd2f0233bca8185c5497269e72b165c5b125b00
carden <carden.ruby@gmail.com>
2020-02-26 00:08:45 +0300

    monotonically increase

--- zadachi/22.rb
+++ zadachi/22.rb
@@ -3,4 +3,4 @@
 arr = [1, 4, 2, 4, 6, 7, -1, 0, 22]
 loc = []
 arr.each_with_index { |_, i| loc << arr[i] if arr[i] > arr[i - 1] }
-puts "Number of monotonously increasing sections #{loc.size}"
+puts "Number of monotonously increasing sections: #{loc.size}"
