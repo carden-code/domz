b467e299b7a0fa2df27d54e31c27922c3121530d
carden <carden.ruby@gmail.com>
2020-02-25 23:11:50 +0300

    modification

--- zadachi/18.rb
+++ zadachi/18.rb
@@ -1,3 +1,4 @@
+# The program loops through the array and determines the number of local maxima.
 arr = [1, 4, 2, 4, 6, 7, -1, 0, 22]
 max = []
 arr.each_with_index { |_, i| max << arr[i] if arr[i - 1] <= arr[i] }
