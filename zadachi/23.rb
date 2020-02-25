f6b8f2a38fb4ea113124d5f674bee2827512cb26
carden <carden.ruby@gmail.com>
2020-02-26 00:14:14 +0300

    monotonously decrease

---
+++ zadachi/23.rb
@@ -0,0 +1,6 @@
+# The program determines the number of sections
+# in which the elements of the array monotonously decrease.
+arr = [1, 4, 2, 4, 6, 7, -1, 0, 22]
+loc = []
+arr.each_with_index { |_, i| loc << arr[i] if arr[i] < arr[i - 1] }
+puts "Number of monotonously increasing sections: #{loc.size}"
