067290b0fae72a461ba26e1a62809dc8dc64c96d
carden <carden.ruby@gmail.com>
2020-02-26 21:33:26 +0300

    comment +

--- zadachi/17.rb
+++ zadachi/17.rb
@@ -1,3 +1,6 @@
+# the program checks the elements of the array whether
+# they form a geometric progression,
+# if so, then displays the denominator of the progression, if not displays nil
 arr = [2, 1, 2, 4, 6, 7, -1, 0, 22]
 den = arr[1] / arr[0]
 puts arr.size > 2 && arr[1] == (arr[0] * den) ? "Denominator:#{den}" : 'nil'
