a0c59c35322fb8769c9934964abca531937a0573
carden <carden.ruby@gmail.com>
2020-02-26 22:09:52 +0300

    bug fix

--- zadachi/17.rb
+++ zadachi/17.rb
@@ -3,4 +3,8 @@
 # if so, then displays the denominator of the progression, if not displays nil
 arr = [2, 1, 2, 4, 6, 7, -1, 0, 22]
 den = arr[1] / arr[0]
-puts arr.size > 2 && arr[1] == (arr[0] * den) ? "Denominator:#{den}" : 'nil'
+if arr.size > 2 && arr[1] == arr[0] * den
+  puts "Denominator:#{den}"
+else
+  puts 'nil'
+end
