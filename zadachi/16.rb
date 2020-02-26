e7bc121d4a00b19f4cf903cb805472b68dc421b9
carden <carden.ruby@gmail.com>
2020-02-26 22:12:16 +0300

    fix bug

--- zadachi/16.rb
+++ zadachi/16.rb
@@ -1,4 +1,8 @@
 # The program checks if the elements of the array form an arithmetic progression
 # If yes, it outputs the difference; if not, displays nil
 a = [2, 1, 2, 4, 6, 7, -1, 0, 22]
-p a.size > 2 && a[1] == (a[0] + a[2]) / 2.0 ? "difference:#{a[1] - a[0]}" : nil
+if a.size > 2 && a[1] == (a[0] + a[2]) / 2.0
+  puts "difference: #{a[1] - a[0]}"
+else
+  puts 'nil'
+end
