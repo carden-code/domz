dd068359d6b22a3177a1a29d1a777d936b66a4cb
carden <carden.ruby@gmail.com>
2020-02-26 21:10:59 +0300

    a => arr

--- zadachi/13.rb
+++ zadachi/13.rb
@@ -1,5 +1,5 @@
 # The program changes all negative elements of the array
 # to the value of the max element.
-a = [2, 1, 2, -2, 0, 4, -6, 9, 8]
-product = a.each_with_index { |_, i| a[i] = a.max if a[i].negative? }
+arr = [2, 1, 2, -2, 0, 4, -6, 9, 8]
+product = arr.each_with_index { |_, i| arr[i] = arr.max if arr[i].negative? }
 puts "Modified array : #{product}"
